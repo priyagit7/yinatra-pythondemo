3c4a1da422b600cc2ec739eb2baf40b2797c3d95
Mohanapriya S <smohanapriyaoffical@gmail.com>
2026-05-05 19:04:32 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [{
-    name: 'yinatra-pythondemo',
-    script: 'gunicorn',
+    name: 'yinatra-pm2-deploy',
+    script: '/opt/apps/yinatra-pm2-deploy/venv/bin/gunicorn',
     args: '--workers 2 --bind 0.0.0.0:3000 app:app',
-    cwd: '/opt/apps/yinatra-pythondemo',
-    interpreter: 'python3',
+    cwd: '/opt/apps/yinatra-pm2-deploy',
+    interpreter: 'none',
     env: {
       FLASK_ENV: 'production',
-      PORT: 3000
+      PORT: '3000'
     }
   }]
 }
